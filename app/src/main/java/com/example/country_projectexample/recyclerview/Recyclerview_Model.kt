63d1a18f6bbe3ebd8327f80a94d8b1e8b2582367
meenjoon <zqudwnsz@naver.com>
2023-01-24 21:46:47 +0900

    DOCS(ADD): Recyclerview 사용을 위한 Data Model Class

---
+++ app/src/main/java/com/example/country_projectexample/recyclerview/Recyclerview_Model.kt
@@ -0,0 +1,12 @@
+package com.example.country_projectexample.recyclerview
+
+data class Recyclerview_Model(
+
+    val capital: String,
+
+    val name: String?,
+
+    val translations: String?,
+
+    val flags: String?
+)
