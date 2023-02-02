923b1919a57af27672c08e6a33c386349511cb22
meenjoon <zqudwnsz@naver.com>
2023-02-02 14:52:10 +0900

    DOCS(Fix): CountriesAPI의 필터링을 이용해 특정 나라의 대한 정보를 얻어오기 위한 getDetailCountries()함수[@Path 이용] 추가

--- app/src/main/java/com/example/country_projectexample/retrofit/CountriesApi.kt
+++ app/src/main/java/com/example/country_projectexample/retrofit/CountriesApi.kt
@@ -1,11 +1,21 @@
 package com.example.country_projectexample.retrofit
 
 
+import com.example.country_projectexample.datamodel_detail.Country_Response_Detail
 import com.example.country_projectexample.datamodel.Country_Response
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CountriesApi {
     @GET("v3.1/all?fields=name,capital,translations,flags")
     fun getCountries(): Call<Country_Response>
+
+//    @GET("v3.1/name/??")
+//    fun getDetailCountries(enName_Common:String) : Call<Country_Response>
+
+    @GET("v3.1/name/{countries-name}")
+    fun getDetailCountries(
+        @Path("countries-name") name: String
+    ) : Call<Country_Response_Detail>
 }
