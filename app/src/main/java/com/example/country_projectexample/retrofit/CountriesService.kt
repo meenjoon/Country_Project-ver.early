d6fd1868898f29a1dc197944152aadb4cd9e7a86
meenjoon <zqudwnsz@naver.com>
2023-02-02 15:02:48 +0900

    DOCS(Fix): OkHttpClient의 TrustManager를 사용하여 TLS/SSL의 인증서의 자격 증명(인증서 체인)을 검사 기능추가

--- app/src/main/java/com/example/country_projectexample/retrofit/CountriesService.kt
+++ app/src/main/java/com/example/country_projectexample/retrofit/CountriesService.kt
@@ -1,8 +1,13 @@
 package com.example.country_projectexample.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object CountriesService {
     val BASE_URL = "https://restcountries.com"
@@ -10,16 +15,36 @@ object CountriesService {
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(getUnsafeOkHttpClient().build())
         .build()
 
+    val retrofitService: CountriesApi = retrofit.create(CountriesApi::class.java)
 
+    fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
 
+            }
 
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
 
+            }
 
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        })
 
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
 
+        val sslSocketFactory = sslContext.socketFactory
 
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        builder.hostnameVerifier { hostname, session -> true }
 
+        return builder
+    }
 
 }
