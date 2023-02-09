fa1d14e93d9a54408a99bb6a41288ea96dd21cc3
zqudwnsz@naver.com <zqudwnsz@naver.com>
2023-02-09 16:12:46 +0900

    DOCS(Fix): 나라에 대한 자세한 정보(GoogleMap,flag,korName,enName,nativeName,cca3,continent,capital,population,currency,language)를 보여주는 액티비티, 뒤로가기를 누를시 다시금 국가 리스트가 보여짐, cioc -> cca3 수정, fidViewById -> ViewBinding 적용

--- app/src/main/java/com/example/country_projectexample/CountriesDetail.kt
+++ app/src/main/java/com/example/country_projectexample/CountriesDetail.kt
@@ -15,49 +15,45 @@ import android.widget.TextView
 import androidx.annotation.NonNull
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
+import com.example.country_projectexample.databinding.ActivityCountriesDetailBinding
 import com.example.country_projectexample.datamodel_detail.Country_Response_Detail
 import com.example.country_projectexample.retrofit.CountriesService.retrofitService
 import retrofit2.Call
 import retrofit2.Response
 
+// <viewBinding 적용 버전>
 class CountriesDetail : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCountriesDetailBinding
 
     @SuppressLint("MissingInflatedId", "WrongViewCast", "CutPasteId", "SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_countries_detail)
+
+        binding = ActivityCountriesDetailBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
 
         val enName_Common = intent.getStringExtra("enName_Common").toString() //리사이클러뷰의 나라를 클릭하면 해당하는 나라의 enName_Common의 값을 intent를 통해 가지고 옴
         val enName_Official = intent.getStringExtra("enName_Official").toString() //리사이클러뷰의 나라를 클릭하면 해당하는 나라의 enName_Official 값을 intent를 통해 가지고 옴
         val korName_Common = intent.getStringExtra("korName_Common").toString() //리사이클러뷰의 나라를 클릭하면 해당하는 나라의 korName_Common 값을 intent를 통해 가지고 옴
         val flag = intent.getStringExtra("flag").toString() //리사이클러뷰의 나라를 클릭하면 해당하는 나라의 korName_Common 값을 intent를 통해 가지고 옴
 
-        val backBtn_Detail = findViewById<ImageView>(R.id.backBtn_Detail)
-        val image_GoogleMap = findViewById<WebView>(R.id.image_GoogleMap)
-        val country_Flag_Detail = findViewById<ImageView>(R.id.country_Flag_Detail)
-        val korName_Detail = findViewById<TextView>(R.id.korName_Detail)
-        val enName_Official_Detail = findViewById<TextView>(R.id.enName_Official_Detail)
-        val nativeNameCommon_Input_Detail = findViewById<TextView>(R.id.nativeNameCommon_Input_Detail)
-        val cioc_Input_Detail = findViewById<TextView>(R.id.cioc_Input_Detail)
-        val continent_Input_Detail = findViewById<TextView>(R.id.continent_Input_Detail)
-        val capital_Input_Detail = findViewById<TextView>(R.id.capital_Input_Detail)
-        val population_Input_Detail = findViewById<TextView>(R.id.population_Input_Detail)
-        val currency_Input_Detail = findViewById<TextView>(R.id.currency_Input_Detail)
-        val language_Input_Detail = findViewById<TextView>(R.id.language_Input_Detail)
-
         /*
             WebView 사용을 위한 설정
          */
-        image_GoogleMap.settings.javaScriptEnabled = true
-        image_GoogleMap.webViewClient = WebViewClient()
-        image_GoogleMap.webChromeClient = WebChromeClient()
+        binding.imageGoogleMap.settings.javaScriptEnabled = true
+        binding.imageGoogleMap.webViewClient = WebViewClient()
+        binding.imageGoogleMap.webChromeClient = WebChromeClient()
 
 
-        korName_Detail.text = korName_Common //클릭시 해당 리사이클러뷰의 값 korName_Common을 intent로 받은 후 korName_Detail에 넣어준다.
-        enName_Official_Detail.text = enName_Official //클릭시 해당 리사이클러뷰의 enName_Official을 intent로 받은 후 enName_Official에 넣어준다.
+        binding.korNameDetail.text = korName_Common //클릭시 해당 리사이클러뷰의 값 korName_Common을 intent로 받은 후 korName_Detail에 넣어준다.
+        binding.enNameOfficialDetail.text = enName_Official //클릭시 해당 리사이클러뷰의 enName_Official을 intent로 받은 후 enName_Official에 넣어준다.
         Glide.with(this) //클릭시 해당 리사이클러뷰의 flag를 intent로 받은 후 Glide를 통해 country_Flag_Detail에 넣어준다.
             .load(flag)
-            .into(country_Flag_Detail)
+            .into(binding.countryFlagDetail)
 
         /*
         object 클래스로부터 retrofitService를 가져온다. 이 retrofitService는 CountriesApi 형태로 반환이 되는데 CountriesApi는 인터페이스 형식이다.
@@ -89,7 +85,7 @@ class CountriesDetail : AppCompatActivity() {
 
                         val nativeNameTolanguage = it.name?.nativeName.toString().substring(it.name?.nativeName.toString().indexOf("common")-4,it.name?.nativeName.toString().indexOf("common")-1) //nativeName으로부터 language에 입력될 언어 필드를 빼옴
 
-                        val cioc = it.cioc //activity_countries_detail.xml 사용 할 cioc(국가코드) 변수
+                        val cca3 = it.cca3 //activity_countries_detail.xml 사용 할 cioc(국가코드) 변수
 
                         val continent = it.region //activity_countries_detail.xml 사용 할 continent(대륙) 변수
 
@@ -99,14 +95,14 @@ class CountriesDetail : AppCompatActivity() {
                         }
                         else {
                             val capital = it.capital.toString().substring(1,it.capital.toString().length-1) //activity_countries_detail.xml 사용 할 capital(수도) 변수
-                            capital_Input_Detail.text = capital //capital_Input_Detail이라는 TextView와 capital 변수 연결
+                            binding.capitalInputDetail.text = capital //capital_Input_Detail이라는 TextView와 capital 변수 연결
                         }
 
                         val population = it.population //activity_countries_detail.xml 사용 할 population(인구) 변수
 
                         //통신으로 받아온 currencies 데이터 정제
                         if (it.currencies==null) {
-                            currency_Input_Detail.text = "" //currency_Input_Detail이라는 TextView와 currency 변수 연결
+                            binding.currencyInputDetail.text = "" //currency_Input_Detail이라는 TextView와 currency 변수 연결
                         } else {
                             val currency_Index1 = it.currencies.toString().indexOf("name") + 5
                             val currency_Index2 = it.currencies.toString().indexOf(",",currency_Index1)
@@ -117,30 +113,31 @@ class CountriesDetail : AppCompatActivity() {
                             val currencies_Symbol = it.currencies.toString().substring(currency_Index3,currency_Index4)
                             val currency = StringBuilder().append(currencies_Symbol).append("(").append(currencies_Name).append(")").toString() //activity_countries_detail.xml 사용 할 currency(화폐) 변수
                             println("currency_Input : ${currency}")
-                            currency_Input_Detail.text = currency //currency_Input_Detail이라는 TextView와 currency 변수 연결
+                            binding.currencyInputDetail.text = currency //currency_Input_Detail이라는 TextView와 currency 변수 연결
                         }
 
-                        nativeNameCommon_Input_Detail.text = nativeName_Common //nativeNameCommon_Input_Detail이라는 TextView와 nativeName_Common 변수 연결
-                        cioc_Input_Detail.text = cioc //cioc_Input_Detail이라는 TextView와 cioc 변수 연결
-                        continent_Input_Detail.text = continent //continent_Input_Detail이라는 TextView와 continent 변수 연결
-                        population_Input_Detail.text = population.toString() //population_Input_Detail이라는 TextView와 population 변수 연결
-                        it.maps?.openStreetMaps.toString().let { googleMapUrl -> image_GoogleMap.loadUrl(googleMapUrl) } //webView에 구글 지도 넣기
+                        binding.nativeNameCommonInputDetail.text = nativeName_Common //nativeNameCommon_Input_Detail이라는 TextView와 nativeName_Common 변수 연결
+                        binding.cca3InputDetail.text = cca3 //cioc_Input_Detail이라는 TextView와 cioc 변수 연결
+                        binding.continentInputDetail.text = continent //continent_Input_Detail이라는 TextView와 continent 변수 연결
+                        binding.populationInputDetail.text = population.toString() //population_Input_Detail이라는 TextView와 population 변수 연결
+                        it.maps?.openStreetMaps.toString().let { googleMapUrl -> binding.imageGoogleMap.loadUrl(googleMapUrl) } //webView에 구글 지도 넣기
 
-                        println("language : ${it.languages.toString()}")
-                        println("language : ${it.languages.toString().indexOf(nativeNameTolanguage)}")
+                        Log.d("language:", it.languages.toString())
+                        Log.d("language", "$nativeNameTolanguage")
+                        Log.d("language","${it.languages.toString().indexOf(nativeNameTolanguage)}")
+
+
 
                         //language 데이터 정제
                        if(it.languages.toString().isEmpty()) {
-
+                            binding.languageInputDetail.text = ""
                         }
-                        else {
+                        else if(it.languages.toString().indexOf(nativeNameTolanguage) !=1) {
                             val languageIndex = it.languages.toString().indexOf(nativeNameTolanguage)
                             val languageIndex2 = it.languages.toString().indexOf(",", languageIndex)
                             val languageSubstring = it.languages.toString().substring(languageIndex,languageIndex2)
-                            language_Input_Detail.text = languageSubstring
+                            binding.languageInputDetail.text = languageSubstring
                         }
-
-
 
                     }
 
@@ -153,7 +150,7 @@ class CountriesDetail : AppCompatActivity() {
 
         })
 
-        backBtn_Detail.setOnClickListener{ //backBtn_Detail 클릭했을 시 뒤로가기
+        binding.backBtnDetail.setOnClickListener{ //backBtn_Detail 클릭했을 시 뒤로가기
             finish()
         }
 
